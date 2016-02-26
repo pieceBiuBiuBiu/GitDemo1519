c261e5a69cdfde31bddeac683976199118b3782a
crs <474067592@qq.com>
2016-02-26 14:20:27 +0800

    work1 创建 preson类

---
+++ GitDemo/GitDemo/preson.h
@@ -0,0 +1,13 @@
+//
+//  preson.h
+//  GitDemo
+//
+//  Created by 崔人帅 on 16/2/26.
+//  Copyright © 2016年 崔人帅. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface preson : NSObject
+
+@end
---
+++ GitDemo/GitDemo/preson.m
@@ -0,0 +1,13 @@
+//
+//  preson.m
+//  GitDemo
+//
+//  Created by 崔人帅 on 16/2/26.
+//  Copyright © 2016年 崔人帅. All rights reserved.
+//
+
+#import "preson.h"
+
+@implementation preson
+
+@end
