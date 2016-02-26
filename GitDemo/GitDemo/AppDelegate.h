3877b743469f9a344ea24b4d29e42481d428580e
crs <474067592@qq.com>
2016-02-26 14:07:58 +0800

    first commit project

---
+++ GitDemo/GitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by 崔人帅 on 16/2/26.
+//  Copyright © 2016年 崔人帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
