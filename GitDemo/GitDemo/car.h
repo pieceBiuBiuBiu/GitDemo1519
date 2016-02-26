4e41119b7bbd715a139a256525bba0c89ae7eba6
crs <474067592@qq.com>
2016-02-26 14:25:16 +0800

    work2 创建一个汽车类

---
+++ GitDemo/GitDemo/car.h
@@ -0,0 +1,13 @@
+//
+//  car.h
+//  GitDemo
+//
+//  Created by 崔人帅 on 16/2/26.
+//  Copyright © 2016年 崔人帅. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface car : NSObject
+
+@end
---
+++ GitDemo/GitDemo/car.m
@@ -0,0 +1,13 @@
+//
+//  car.m
+//  GitDemo
+//
+//  Created by 崔人帅 on 16/2/26.
+//  Copyright © 2016年 崔人帅. All rights reserved.
+//
+
+#import "car.h"
+
+@implementation car
+
+@end
